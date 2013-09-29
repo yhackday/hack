4929bf652a3e5c31f2586d70888ef071a90a300f
R Pooran Prasad <yhackday@yahoo.com>
2013-09-29 01:29:15 -0700

    new changes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var hbs = require('express3-handlebars');
+// Authenticate via OAuth
+var tumblr = require('tumblr.js');
+var client = tumblr.createClient({
+  consumer_key: 'lQS712YUjSnDppww0NV8EcFn1Y4Ch7p6cSnUB4g11MKt314hJs',
+  consumer_secret: 'fXC9IhAMyr91jzS437ycXrw4GdI5fHyI3ZesWCpacZ7zS1EILL',
+  token: 'TPKDdyc8YyF96KMUeSW4hsyBi60DgcD5iifrcmvrn3QmxWigDx',
+  token_secret: 'YHpiKabYcjCRfEVfu3nadWOfCsIo2GfJDpfTMLpqCMwZ77tI4t'
+});
 
-var app = express();
-
-app.engine('handlebars', hbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function(req, res) {
-    res.render('index');
+// Make the request
+client.posts('pooran.tumblr.com', function (err, data) {
+ alert(data);
 });
 
 var port = process.env.PORT || 5000;
